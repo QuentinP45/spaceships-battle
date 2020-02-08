7b286ca51a3f2a68899c6ce27681443b07579c8a
quentin.picart <quentin.picart@gmail.com>
2020-02-08 13:43:55 +0100

    mvc(routing): ajout route page d'accueil

--- src/index.php
+++ src/index.php
@@ -15,4 +15,7 @@ switch($requestedController){
     case 'index.php?session-off=1':
         require_once(__DIR__ . '/controllers/indexController.php');
         break;
+    case 'homepage.php':
+        require_once(__DIR__ . '/controllers/homepageController.php');
+        break;
 }
