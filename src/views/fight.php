3d26ba502ff4462d88c81f10894b3447435deac1
quentin.picart <quentin.picart@gmail.com>
2020-02-18 10:04:25 +0100

    mvc(views): création de la vue fight

---
+++ src/views/fight.php
@@ -0,0 +1,31 @@
+<?php
+ob_start();
+?>
+<section id="container">
+    <h1>Combats</h1>
+    <section>
+        <h2>Joueurs prêts pour le combat</h2>
+        <?php if (!$userEstDispo): ?>
+        <p>Vous n'avez pas de vaisseau disponible pour le combat !<br>
+        Modifiez l'activité d'un vaisseau sur votre page d'accueil pour affronter un joueur !</p>
+        <?php endif ?>
+        <div id="available-players">
+        <?php foreach($joueursDisponibles as $joueur):?>
+            <section>
+                <h2><?=$joueur->loginJoueur?></h2>
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkooledge.com%2Fassets%2Fdefault_medium_avatar-57d58da4fc778fbd688dcbc4cbc47e14ac79839a9801187e42a796cbd6569847.png&f=1&nofb=1" alt="">
+                <p>Niveau: <?=$joueur->niveau?></p>
+                <?php if ($userEstDispo): ?>
+                <form action="battleground.php" method="GET">
+                    <button type="submit" name="fight" value="<?=$joueur->idJoueur?>">Affronter</button>
+                </form>
+                <?php endif ?>
+            </section>
+        <?php endforeach?>
+        </div>
+    </section>
+</section>
+<?php
+$content=ob_get_clean();
+
+require_once('views/templates/base_layout.php');
